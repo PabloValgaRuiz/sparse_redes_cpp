e76ff1bf93c9320ee0c9bd445bd765440389d41a
pablo valgañon <pablovalgaruiz@gmail.com>
2021-11-22 20:02:41 +0100

    Crear matriz y laplaciano del grafo

--- sparse_redes.cpp
+++ sparse_redes.cpp
@@ -20,20 +20,43 @@ int main(){
         vecinos.push_back(T.vecinos[i]);
 
     SpMat Matrix(T.N, T.N);
-    std::vector<Triplet> tripletes;
+    SpMat Laplaciano(T.N, T.N);
+    std::vector<Triplet> tripletes1, tripletes2;
 
-    Matrix.reserve(vecinos);
 
-    for(int i = 0; i < T.N; i++){
+    for(int i = 0; i < T.N; i++){ //Construccion de la matriz
+        int laplDiag = 0;
+
         for(int j = 0; j < T.vecinos[i]; j++){
-            double value = (T.Mpesos[i][j] + T.MpesosT[i][j])/2; //media para que quede simetrica
-            tripletes.emplace_back(i, T.Mvecinos[i][j], value);
+            double value = T.Mpesos[i][j];
+            if(i != T.Mvecinos[i][j]){
+                tripletes1.emplace_back(i, T.Mvecinos[i][j], value/2);//mitad en i,j
+                tripletes1.emplace_back(T.Mvecinos[i][j], i, value/2);//mitad en j,i (simetrica)
+            }
+            else
+                tripletes1.emplace_back(i, T.Mvecinos[i][j], value); //diagonal
         }
     }
-    Matrix.setFromTriplets(tripletes.begin(), tripletes.end());
+    for(int i = 0; i < T.N; i++){ //Construccion del laplaciano
+        int laplDiag = 0;
+        for(int j = 0; j < T.vecinos[i]; j++){
+            if(i == T.Mvecinos[i][j]){
+                laplDiag = 1; //Ver si hay un autoloop
+                tripletes2.emplace_back(i,i, T.vecinos[i] - 1);
+            }
+            else if((i > T.Mvecinos[i][j]) && (laplDiag == 0)){
+                tripletes2.emplace_back(i,i, T.vecinos[i]);
+                laplDiag = 1;
+            }
+            else
+                tripletes2.emplace_back(i, T.Mvecinos[i][j], 1);
+        }
+    }
+
+    Matrix.setFromTriplets(tripletes1.begin(), tripletes1.end());
     Matrix.makeCompressed();
-
-
+    Laplaciano.setFromTriplets(tripletes2.begin(), tripletes2.end());
+    Laplaciano.makeCompressed();
 
     return 0;
 }
